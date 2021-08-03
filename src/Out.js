a5c79490fb7034db907e66b1fee5f5a18ed1f45d
disverst <69248760+disverst@users.noreply.github.com>
2021-08-03 23:33:12 +0300

    First commit
    Wrote app using react-redux, @reduxjs/toolkit

---
+++ src/Out.js
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux'
+import {
+	selectValue, 
+	selectText
+} from './app/taskReducerSlice'
+
+export default function Out() {
+
+	const task1 = useSelector(selectValue)
+	const task2 = useSelector(selectText)
+
+	return (
+		<>
+			<div>
+				<hr />
+				<h2>Task 1</h2>
+				<p>{task1}</p>
+				<p>{task2}</p>
+			</div>
+		</>
+	)
+}
